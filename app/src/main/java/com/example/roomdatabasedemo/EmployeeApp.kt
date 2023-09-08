5238c9015739263ff54f988aec29a2b1fcd3fc6b
bekach888 <beketchkoidze@gmail.com>
2023-09-08 23:52:31 +0400

    roomDatabase demo

---
+++ app/src/main/java/com/example/roomdatabasedemo/EmployeeApp.kt
@@ -0,0 +1,11 @@
+package com.example.roomdatabasedemo
+
+import android.app.Application
+
+class EmployeeApp:Application() {
+
+    val db by lazy {
+        EmployeeDatabase.getInstance(this)
+    }
+
+}
---
+++ app/src/main/java/com/example/roomdatabasedemo/EmployeeDao.kt
@@ -0,0 +1,24 @@
+package com.example.roomdatabasedemo
+
+import androidx.room.*
+import com.example.roomdatabasedemo.EmployeeEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface EmployeeDao {
+
+    @Insert
+    suspend fun insert(employeeEntity: EmployeeEntity)
+
+    @Update
+    suspend fun update(employeeEntity: EmployeeEntity)
+
+    @Delete
+    suspend fun delete(employeeEntity: EmployeeEntity)
+
+    @Query("Select * from `employee-table`")
+    fun fetchAllEmployee():Flow<List<EmployeeEntity>>
+
+    @Query("Select * from `employee-table` where id=:id")
+    fun fetchEmployeeById(id:Int):Flow<EmployeeEntity>
+}
